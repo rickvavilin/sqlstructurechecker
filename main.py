f5e8e1003516b1e3dc7d9fb6af4d54d1d4a8a1e4
Aleksandr Vavilin <vavilin@tcobdd.ru>
2015-06-29 21:34:19 +0500

    add referential constraints check

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class DateTimeEncoder(json.JSONEncoder):
 
 class Differ():
     """
-    Differ is class, that compute 
+    Differ is class, that compute difference of two dicts or lists
     """
     def __init__(self):
         self.diffs = []
@@ -72,7 +72,6 @@ class Differ():
         elif isinstance(i1, list) and isinstance(i2, list):
             self.listdiff(i1, i2, level, keyschain)
         else:
-            #print getindent(level), keyschain, i1, i2
             self.diffs.append(self.creatediff(i1, i2, keyschain, 'differ'))
 
     def dictdiff(self, d1, d2, level, keyschain):
@@ -86,21 +85,17 @@ class Differ():
         for key in d1.iterkeys():
             if key in d2:
                 if d1[key]!=d2[key]:
-                    #print getindent(level), 'differ: ', key, ':'
                     self.itemdiff(d1[key], d2[key], level+1, keyschain+[key])
-
 
     def listdiff(self, l1, l2, level, keyschain):
         for i in xrange(max(len(l1), len(l2))):
             if i < len(l1) and i < len(l2):
                 if l1[i] != l2[i]:
-                    #print getindent(level), 'differ: ', i, ':'
                     self.itemdiff(l1[i], l2[i], level+1, keyschain+[i])
             elif i < len(l1):
                 self.diffs.append(self.creatediff(None, None, keyschain+[i], 'removed'))
             elif i < len(l2):
-                self.diffs.append(self.creatediff(None, None, keyschain+[added], 'removed'))
-
+                self.diffs.append(self.creatediff(None, None, keyschain+[i], 'added'))
 
 
 def savestructure(filename, structure):
@@ -115,7 +110,13 @@ def loadstructure(filename):
     f.close()
     return structure
 
+
 def getitems(query, params, cur, keyfield):
+    """
+    helper function for create dict of dicts from result
+    of query. Keys for dict is values of keyfield column.
+    Keyfield column must be unique in the result of query.
+    """
     cur.execute(query, params)
     result = {}
     for item in cur.fetchall():
@@ -123,10 +124,20 @@ def getitems(query, params, cur, keyfield):
     cur.nextset()
     return result
 
+
 def normalize(structure):
     return json.loads(json.dumps(structure, cls=DateTimeEncoder), cls=DateTimeDecoder)
 
+
 def get_structure_from_database(host='localhost', user='root', passwd='2360087', database=None, **kwargs):
+    """
+    :param host: hostname for database connection
+    :param user: username for database connection
+    :param passwd: password for database connection
+    :param database: database name
+    :param kwargs: for compatibility
+    :return: structure of database metadata, include TABLES, VIEWS, CONSTRAINTS, ROUTINES
+    """
     db = connect(host=host, user=user, passwd=passwd, db='information_schema')
     cur = db.cursor(cursors.DictCursor)
     cur.execute("SET CHARSET 'utf8'")
@@ -139,6 +150,7 @@ def get_structure_from_database(host='localhost', user='root', passwd='2360087',
         tables[table['TABLE_NAME']] = table
         table['COLUMNS'] = getitems('select * from columns where table_schema=%s and table_name=%s', [database_name, table['TABLE_NAME']], cur, 'COLUMN_NAME')
         table['CONSTRAINTS'] = getitems('select * from TABLE_CONSTRAINTS where table_schema=%s and table_name=%s', [database_name, table['TABLE_NAME']], cur, 'CONSTRAINT_NAME')
+        table['REF_CONSTRAINTS'] = getitems('select * from REFERENTIAL_CONSTRAINTS where constraint_schema=%s and table_name=%s', [database_name, table['TABLE_NAME']], cur, 'CONSTRAINT_NAME')
         table['TRIGGERS'] = getitems('select * from TRIGGERS where event_object_schema=%s and event_object_table=%s', [database_name, table['TABLE_NAME']], cur, 'TRIGGER_NAME')
 
     cur.execute('select * from routines where routine_schema=%s', [database_name])
