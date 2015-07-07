53e0c482287969b7f720dc5c20dec70a58099c87
Aleksandr Vavilin <vavilin@tcobdd.ru>
2015-07-07 20:39:04 +0500

    make package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sqlstructurecomparer',
+    version='0.1',
+    packages=['sqlcomparer'],
+    url='',
+    license='',
+    author='Aleksandr Vavilin',
+    author_email='rickvavilin@gmail.com',
+    description='tool for compare sql database structures'
+)
