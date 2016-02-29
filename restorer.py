b75a75713109ddd4bdbcc881a834fb4a94dc7516
Aleksandr Vavilin <vavilin@tcobdd.ru>
2016-02-29 15:53:53 +0500

    remove dumper restorer

--- restorer.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'rick'
-from sqlcomparer import dumprestore
-
-if __name__=='__main__':
-    d = dumprestore.MyDumpRestore('127.0.0.1', 'root', '2360087', './160', 'odissey_new')
-    d.restore('all')
-    print d.errors
